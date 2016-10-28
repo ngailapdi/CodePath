8c0e74b66644e908d3eff11b793e3b31e85f7312
ngailapdi <ngailapdi@gmail.com>
2016-10-28 17:13:25 -0400

    EnumerateAllPrimes

---
+++ EnumerateAllPrimes.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class EnumerateAllPrimes {
+    public int countPrimes(int n) {
+        if (n < 3) return 0;
+        boolean[] arr = new boolean[n];
+        int res = 0;
+        for (int i = 2; i < n; i++) {
+            if (!arr[i]) {
+               res++;
+               for (int j = 2; j*i < n; j ++) {
+                  arr[j*i] = true;
+               }
+            }
+        }
+        return res;
+    }
+    public static void main(String[] args) {
+        int n = 50;
+        EnumerateAllPrimes o = new EnumerateAllPrimes();
+        System.out.println(o.countPrimes(50));
+    }
+}
